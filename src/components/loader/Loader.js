57aa03c23b9e71d4465f7a469a1bcae318db491f
Damian <d.pagowski@gmail.com>
2020-02-04 13:47:07 +0700

    loading reducer

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Loader.css";
+export default function Loader() {
+  return (
+    <div className="loading">
+      <i className="fa fa-cog fa-spin" />
+    </div>
+  );
+}
